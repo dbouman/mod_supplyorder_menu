1ad1f63cc0aa467a70cd93cbad9a8451e31205b1
Danny Bouman <dannybb@gmail.com>
2012-08-09 14:11:26 -0400

    Update template to include links

--- tmpl/default.php
+++ tmpl/default.php
@@ -7,16 +7,62 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 if ($role_id == 4) {
 ?>
 Accounting
+<ul>
+	<li>
+		<a href="<?php echo JRoute::_( 'index.php?option=com_supplyorder&view=list&layout=pendingAccounting' ); ?>">
+			<?php echo JText::_('Pending Order'); ?>
+		</a>
+	</li>
+	<li>
+		<a href="<?php echo JRoute::_( 'index.php?option=com_supplyorder&view=list&layout=ordered' ); ?>">
+			<?php echo JText::_('Ordered'); ?>
+		</a>
+	</li>
+</ul>
 <?php 
 }
 ?>
 
-<?php 
-if ($role_id == 3 || $role_id == 2) {
+<?php
+// Tier 1 & 2 Approval 
+if ($role_id == 2 || $role_id == 3) {
 ?>
-Tier 1 & 2 Approval
+Approval
+<ul>
+	<li>
+		<a href="<?php echo JRoute::_( 'index.php?option=com_supplyorder&view=list&layout=pending' ); ?>">
+			<?php echo JText::_('Pending'); ?>
+		</a>
+	</li>
+	<li>
+		<a href="<?php echo JRoute::_( 'index.php?option=com_supplyorder&view=list&layout=approved' ); ?>">
+			<?php echo JText::_('Approved'); ?>
+		</a>
+	</li>
+</ul>
 <?php 
 }
 ?>
 
-Registered
+<ul>
+	<li>
+		<a href="<?php echo JRoute::_( 'index.php?option=com_supplyorder' ); ?>">
+			<?php echo JText::_('New Request'); ?>
+		</a>
+	</li>
+	<li>
+		<a href="<?php echo JRoute::_( 'index.php?option=com_supplyorder&view=list&layout=saved' ); ?>">
+			<?php echo JText::_('Saved'); ?>
+		</a>
+	</li>
+	<li>
+		<a href="<?php echo JRoute::_( 'index.php?option=com_supplyorder&view=list&layout=requested' ); ?>">
+			<?php echo JText::_('Requested'); ?>
+		</a>
+	</li>
+	<li>
+		<a href="<?php echo JRoute::_( 'index.php?option=com_supplyorder&view=list&layout=received' ); ?>">
+			<?php echo JText::_('Received'); ?>
+		</a>
+	</li>
+</ul>
